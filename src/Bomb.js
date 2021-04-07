669ef7c9366499faa5836581aad5f56ea75dc97e
Wayne del Corral Jr <wdelcorjr@gmail.com>
2021-04-06 23:11:36 -0500

    commit tree 7d92103f5a7259ffb045de3859172f9266b2f800

--- src/Bomb.js
+++ src/Bomb.js
@@ -1,1 +1,25 @@
 // your Bomb code here!
+import React from 'react';
+
+export default class Bomb extends React.Component {
+    constructor(props) {
+        super()
+        this.state = {
+          secondsLeft: props.initialCount
+        }
+    }
+
+    initiateBomb = () => {
+        if (this.state.secondsLeft > 0) {
+            return (<div>{this.state.secondsLeft} seconds left before I go boom!</div>)
+        } else {
+            return (<div>Boom!</div>)
+        }
+    }
+
+    render() {
+        return (
+           <div>{ this.initiateBomb() }</div> 
+        )
+    }
+}
